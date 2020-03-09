ba9ad2ada9b26871854e404a9b76c6d2140ea0e0
phuc_dev <hphuc5453@gmail.com>
2020-03-09 14:50:03 +0700

    Update base

--- project_1/app/src/main/java/com/example/test/ui/screen/test_base/presentation/renderer/TestBaseRenderer.kt
+++ project_1/app/src/main/java/com/example/test/ui/screen/test_base/presentation/renderer/TestBaseRenderer.kt
@@ -6,9 +6,11 @@ import com.example.test.R
 import com.example.test.core.app.util.image.GlideImageHelper
 import com.example.test.ui.screen.test_base.presentation.model.TestBaseViewModel
 import kotlinx.android.synthetic.main.item_layout_test_base.view.*
+import vn.minerva.core.app.domain.excecutor.EventFireUtil
+import vn.minerva.core.base.domain.listener.OnActionData
 import vn.minerva.core.base.presentation.mvp.android.model.ViewRenderer
 
-class TestBaseRenderer (context: Context): ViewRenderer<TestBaseViewModel>(context){
+class TestBaseRenderer (context: Context, private val onaction : OnActionData<TestBaseViewModel>): ViewRenderer<TestBaseViewModel>(context){
     override fun getLayoutId(): Int {
         return R.layout.item_layout_test_base
     }
@@ -17,6 +19,7 @@ class TestBaseRenderer (context: Context): ViewRenderer<TestBaseViewModel>(conte
 
     override fun bindView(model: TestBaseViewModel, viewRoot: View) {
         viewRoot.tvTestBase.text = model.name
+        EventFireUtil.fireEvent(onaction, model)
     }
 
 }
