8c3b6d41447a3b7caa78d205c3a359ef9b187561
phuc_dev <hphuc5453@gmail.com>
2020-03-09 14:57:31 +0700

    Update base

--- project_1/app/src/main/java/com/example/test/ui/screen/test_base/presentation/model/TestBaseViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.test.ui.screen.test_base.presentation.model
-
-import com.github.vivchar.rendererrecyclerviewadapter.ViewModel
-
-class TestBaseViewModel (
-    var name : String
-): ViewModel
