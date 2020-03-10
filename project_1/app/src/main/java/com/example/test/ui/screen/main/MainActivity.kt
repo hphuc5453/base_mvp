16712a4d244aacac7106b73adde407900f55443d
phuc_dev <hphuc5453@gmail.com>
2020-03-10 08:51:18 +0700

    Update base

--- project_1/app/src/main/java/com/example/test/ui/screen/main/MainActivity.kt
+++ project_1/app/src/main/java/com/example/test/ui/screen/main/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.example.test.ui.screen.test_base
+package com.example.test.ui.screen.main
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.test.R
 import com.example.test.ui.screen.main.presentation.MainView
 import vn.minerva.core.base.presentation.mvp.android.AndroidMvpView
 import vn.minerva.core.base.presentation.mvp.android.MvpActivity
 
-class TestBaseActivity : MvpActivity(){
+class MainActivity : MvpActivity(){
     override fun createAndroidMvpView(): AndroidMvpView {
         return MainView(this, MainView.ViewCreator(this, null))
+    }
+
+    override fun getBackgroundScreen(): Int {
+        return R.drawable.header_full
     }
 }
